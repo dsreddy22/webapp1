c47ddc14a5970ece1342353fee0318f2255e1682
dsreddy22 <32877301+dsreddy22@users.noreply.github.com>
2018-10-05 16:58:05 -0400

    testing the code

--- inputs.tf
+++ inputs.tf
@@ -7,6 +7,6 @@ variable "random-id-name" {
 
 variable "app_service_plan_id" {
   type = "string"
-  description = "The id will come from the appservice plan"
+  description = "The id will the appservice plan"
   default = "/subscriptions/84adf927-45fd-4ca8-82a5-59ccda2785f5/resourceGroups/testwebapp83/providers/Microsoft.Web/serverFarms/appserviceplan"
 }
